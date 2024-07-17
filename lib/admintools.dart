9a9d5ed6874e1e8d8e31b68cceae685b1ed58eaf
nipun <nipundilshan991@gmail.com>
2024-07-17 15:34:01 +0530

    menu icons

--- lib/admintools.dart
+++ lib/admintools.dart
@@ -11,12 +11,53 @@ class toolspage extends StatefulWidget {
 class _toolspageState extends State<toolspage> {
   @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
       body: Background(
           child: Container(
         child: Column(children: <Widget>[
+          SizedBox(
+            height: size.height * 0.07,
+          ),
           Row(
-            children: <Widget>[],
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: <Widget>[
+              Container(
+                width: size.width * 0.4,
+                height: size.height * 0.15,
+                padding: EdgeInsets.only(
+                    left: size.width * 0.03,
+                    top: size.width * 0.02,
+                    bottom: size.width * 0.02,
+                    right: size.width * 0.03),
+                margin: EdgeInsets.only(
+                    left: size.width * 0.03,
+                    bottom: size.width * 0.03,
+                    right: size.width * 0.03),
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(119, 187, 162, 1.0),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Text("mm"),
+              ),
+              Container(
+                width: size.width * 0.4,
+                height: size.height * 0.15,
+                padding: EdgeInsets.only(
+                    left: size.width * 0.03,
+                    top: size.width * 0.02,
+                    bottom: size.width * 0.02,
+                    right: size.width * 0.03),
+                margin: EdgeInsets.only(
+                    left: size.width * 0.03,
+                    bottom: size.width * 0.03,
+                    right: size.width * 0.03),
+                decoration: BoxDecoration(
+                  color: const Color.fromRGBO(119, 187, 162, 1.0),
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ],
           )
         ]),
       )),
