d9b182672ea9b18ce3b1296e34f79197aa15609f
nipun <nipundilshan991@gmail.com>
2024-07-20 10:41:41 +0530

    search by mobile number

--- lib/users/allusers.dart
+++ lib/users/allusers.dart
@@ -15,6 +15,8 @@ class allusers extends StatefulWidget {
 
 class _allusersState extends State<allusers> {
   Future<List<User>> users = getallusers();
+  TextEditingController _searchController = TextEditingController();
+  String _searchQuery = "";
 
   static Future<List<User>> getallusers() async {
     final response = await http.get(Uri.parse(getusers));
@@ -24,9 +26,20 @@ class _allusersState extends State<allusers> {
 
   @override
   void initState() {
-    //TODO: implement initState
+    // TODO: implement initState
     super.initState();
+    _searchController.addListener(() {
+      setState(() {
+        _searchQuery = _searchController.text;
+      });
+    });
     getallusers();
+  }
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
   }
 
   @override
@@ -36,6 +49,29 @@ class _allusersState extends State<allusers> {
       body: Background(
           child: Container(
         child: Column(children: [
+          SizedBox(
+            height: size.height * 0.08,
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 16.0),
+            child: TextField(
+              controller: _searchController,
+              decoration: const InputDecoration(
+                hintText: 'Search by mobile number',
+                prefixIcon: Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                ),
+                focusedBorder: OutlineInputBorder(
+                  borderRadius: BorderRadius.all(Radius.circular(25.0)),
+                  borderSide: BorderSide(
+                      color: Color.fromRGBO(
+                          60, 121, 98, 1.0) // Change to your desired color
+                      ),
+                ),
+              ),
+            ),
+          ),
           SizedBox(
             height: size.height * 0.03,
           ),
@@ -50,7 +86,10 @@ class _allusersState extends State<allusers> {
                             ConnectionState.waiting) {
                           return const CircularProgressIndicator.adaptive();
                         } else if (snapshot.hasData) {
-                          final users = snapshot.data!;
+                          final users = snapshot.data!
+                              .where((user) =>
+                                  user.mobile_number.startsWith(_searchQuery))
+                              .toList();
 
                           return buildusers(users);
                         } else {
